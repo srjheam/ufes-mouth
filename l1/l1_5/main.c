bf3b92f57a6705f5aeaeeed7398e7d762431b74d
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-05-01 00:07:28 -0300

    Submit solution l1_5

---
+++ l1/l1_5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(void) {
+    int n1 = 0, n2 = 0;
+
+    scanf("%d %d", &n1, &n2);
+
+    if (n1 > n2) {
+        printf("N1 = %d", n1);
+    }
+    else if (n2 > n1)
+    {
+        printf("N2 = %d", n2);
+    }
+    else {
+        printf("N1 = N2");
+    }
+}
