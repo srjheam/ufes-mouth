6a7eb685a7dd3b6ecbac437bdca952157c1ded98
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-05-05 09:13:25 -0300

    Submit solution l2_9

---
+++ l2/l2_9/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void main(void) {
+    int n = 0, m = 0;
+
+    scanf("%d %d", &n, &m);
+
+    /* Sieve of Eratosthenes */
+    
+    int sieve[m + 1];
+    memset(sieve, 1, (m + 1)*sizeof(int));
+    /* Populate an Array, indexed from 2 to m, with true values */
+
+    int i;
+    for (i = 2; i <= sqrt(m); i++)
+    {
+        if (sieve[i])
+        {
+            int j;
+            for (j = pow(i, 2); j <= m; j += i)
+            {
+                sieve[j] = 0;
+            }
+        }
+    }
+
+    for (i = n + 1; i < m; i++)
+    {
+        if (sieve[i])
+        {
+            printf("%d\n", i);
+
+            int j;
+            for (j = 2; i * j < m; j++)
+            {
+                printf("%d ", i * j);
+            }
+
+            if (i * 2 >= m)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+    }
+}
