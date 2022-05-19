e9ba99fa9c6bf6c56dd9edc176e363d8d6bdac17
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-05-19 00:09:46 -0300

    Submit solution l3_3

---
+++ l3/l3_3/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int EhPrimo(int num) {
+    /* Sieve of Eratosthenes */
+    
+    int sieve[num + 1];
+    memset(sieve, 1, (num + 1)*sizeof(int));
+    /* Populate an Array, indexed from 2 to n, with true values */
+
+    int i;
+    for (i = 2; i <= sqrt(num); i++)
+    {
+        if (sieve[i])
+        {
+            int j;
+            for (j = pow(i, 2); j <= num; j += i)
+            {
+                sieve[j] = 0;
+            }
+        }
+    }
+
+    return sieve[num];
+}
+
+void main(void) {
+    int min = 0, max = 0;
+    scanf("%d %d", &min, &max);
+
+    int i;
+    for (i = min + 1; i < max; i++)
+    {
+        if (EhPrimo(i))
+        {
+            printf("%d ", i);
+        }
+    }
+}
